9d32cafc4035c1a936688658c2d555ba73756b06
unknown <athulrajkdlr@gmail.com>
2023-03-13 03:20:17 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//function add(a,b){
+   // var c = a+b;
+    
+   // console.log(c)
+//}
+//add(1,2);
+//add(2,3);
+
+//const addition =(a,b)=>{
+   // var c =a+b;
+
+   // console.log(c)
+
+//}
+//addition(1,2)
+
+//const multi=(a,b)=>{
+    //var c=a*b;
+    //console.log(c)
+//}
+//multi(5,2)
+//var x=20;
+//if(x>20){
+    //console.log("good");
+//}
+//else
+//console.log("bad"); 
+
+//for(var i=1;i<=5;i++)
+//{
+    //console.log(i);
+//}
+//var fruits =["apple","orange","mango","grape"];
+//for(let i=0;i<fruits,length;i++){
+    //console.log(fruits[i])
+//}
+
+//let p=0;
+//while(p<fruits.length){
+   // console.log(fruits[p]);
+    //p++;
+//}
+ //let i=0;
+ //do{
+   // console.log(fruits[i]);
+    //i++
+// }while(i<fruits.length)    
